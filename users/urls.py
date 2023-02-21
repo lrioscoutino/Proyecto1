bd9d09d9b3e1d8ba5d3940d5a6549351acd80f59
Luis Rios Coutino <lrioscoutino@gmail.com>
2023-02-20 18:30:46 -0600

    Second commit

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from users.views import home, about
+from users.views import home, about, add_student, edit_student
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('home/', home, name='home_view'),
+    path('add-student/', add_student, name='add_student_view'),
+    path('edit-student/<int:student_id>/', edit_student, name='edit_student_view'),
     path('about/', about, name='about_view'),
     path('', LoginView.as_view(template_name='base.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout')
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from users.models import Students
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -16,8 +17,34 @@ def home_view(request):
 
 def home(request):
     students = Students.objects.all()
-    return render(request, 'home.html', {'students':students})
+    return render(request, 'home.html', {'students': students})
 
 
 def about(request):
     return render(request, 'about.html')
+
+
+def add_student(request):
+    if request.method == 'POST':
+        student = Students()
+        student.name = request.POST.get('name')
+        student.address = request.POST.get('address')
+        student.birth_date = request.POST.get('birth_date')
+        student.status = True
+        student.save()
+        return reverse_lazy('home_view')
+    else:
+        return render(request, "users/form_add.html")
+
+
+def edit_student(request, student_id):
+    student = Students.objects.get(id=student_id)
+    if request.method == 'POST':
+        student.name = request.POST.get('name')
+        student.address = request.POST.get('address')
+        student.birth_date = request.POST.get('birth_date')
+        student.status = True
+        student.save()
+
+    else:
+        return render(request, "users/form_add.html", {'student': student})
